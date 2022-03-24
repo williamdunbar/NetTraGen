d794ab8be91f14d8982da437561ae3f3ada05149
William <williamdunbar2000@gmail.com>
2022-03-24 18:41:16 +0700

    Update select html

--- template/assets/js/script.js
+++ template/assets/js/script.js
@@ -1,25 +1,25 @@
 
-let selectPhuongPhap_TC = document.getElementById('phuongphaptancong');
-let selectLoai_TC = document.getElementById('loaitancong');
+// let selectPhuongPhap_TC = document.getElementById('phuongphaptancong');
+// let selectLoai_TC = document.getElementById('loaitancong');
 
-function checkTypeInput() {
-    let typeInput1 = document.querySelector("#port-n").classList;
-    let typeInput2 = document.querySelector("#port-d").classList;
+// function checkTypeInput() {
+//     let typeInput1 = document.querySelector("#port-n").classList;
+//     let typeInput2 = document.querySelector("#port-d").classList;
 
-    if (selectPhuongPhap_TC.value == 'aa' && selectLoai_TC.value == 'aa') {
-        typeInput1.remove("hide");
-        typeInput2.remove("hide")
-    } else {
-        typeInput1.add("hide");
-        typeInput2.add("hide")
-    }
-}
+//     if (selectPhuongPhap_TC.value == 'aa' && selectLoai_TC.value == 'aa') {
+//         typeInput1.remove("hide");
+//         typeInput2.remove("hide")
+//     } else {
+//         typeInput1.add("hide");
+//         typeInput2.add("hide")
+//     }
+// }
 
 
-selectPhuongPhap_TC.onchange = () => {
-    checkTypeInput();
-};
+// selectPhuongPhap_TC.onchange = () => {
+//     checkTypeInput();
+// };
 
-selectLoai_TC.onchange = () => {
-    checkTypeInput();
-}
+// selectLoai_TC.onchange = () => {
+//     checkTypeInput();
+// }
